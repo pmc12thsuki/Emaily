5b86c2c4dba57a3cd7669280b554d3fdcc755711
wang sze chi <pmc12thsuki@gmail.com>
2019-03-11 11:25:42 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(PORT, () => {
+  console.log(`listen on port ${PORT}`);
+});
