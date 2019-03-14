a231c651c4639628fc57b507970dbd5472de4746
wang sze chi <pmc12thsuki@gmail.com>
2019-03-14 14:50:42 +0800

    finish webhook

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,5 +5,7 @@ const localhost = 'http://localhost:5000';
 module.exports = (app) => {
   // 告訴 client 端，如果看到要前往 client_host/auth/google 的要導向 target, 也就是 http://localhost:5000/auth/google
   app.use(proxy('/auth/google', { target: localhost }));
-  app.use(proxy('/api/*', { target: localhost })); // 設定所有 api/ 開頭的 URL 都會通過 proxy
+  app.use(proxy('/api/*', { target: localhost })); // 設定所有 api/ 開頭的 URL 都會通過 proxy (但只有一層？)
+  app.use(proxy('/api/surveys/**/yes', { target: localhost })); 
+  app.use(proxy('/api/surveys/**/no', { target: localhost })); 
 };
--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -34,7 +37,6 @@ module.exports = (app) => {
       // send email by sendgrid, first argument is the data of our email, second is the html of our email
       const mailer = new Mailer(survey, surveyTemplate(survey));
       await mailer.send();
-
       // after email is sent successfully, update Survey and User in DB
       await survey.save();
       req.user.credits -= 1;
@@ -43,5 +45,58 @@ module.exports = (app) => {
     } catch (err) {
       res.status(422).send(err);
     }
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice'); // 我們想要從路徑中 parse 出來的樣式
+    _.chain(req.body) // 使用 lodash 的 chain 功能，把 map, compact, uniqBy 串起來，最後再用 value 回傳處理好的值
+      .map((event) => {
+        const match = p.test(new URL(event.url).pathname); // 幫我們從 url parse 成 { surveyId: '5c89c11141a650a57e23d1c6', choice: 'yes' } 的樣式，如果缺少任一參數就會回覆 null
+        if (match) {
+          return {
+            email: event.email,
+            surveyId: match.surveyId,
+            choice: match.choice,
+          }; // 沒有 match 的 event 就會回傳 undefined
+        }
+      })
+      .compact() // 這個函數會 loop array 並過濾掉那些 undefined 的值。
+      .uniqBy('email', 'surveyId') // 這個函數會移除 array 中 duplicate 的那些（要 email + surveyId 一起重複出現才會被移除）
+      .each(({ surveyId, email, choice }) => { // each query is an async function, but we dont need to wait for it execute
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email, responded: false },
+          },
+        }, {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date(),
+        }).exec();
+      })
+      .value(); // 最後回傳 value
+
+
+    res.send({});
+    /*
+    這段 code 跟上面一樣，只是上面更簡潔
+    const events = req.body.map((event) => {
+      const match = p.test(new URL(event.url).pathname); // 幫我們從 url parse 成 { surveyId: '5c89c11141a650a57e23d1c6', choice: 'yes' } 的樣式，如果缺少任一參數就會回覆 null
+      if (match) {
+        return {
+          email: event.email,
+          surveyId: match.surveyId,
+          choice: match.choice,
+        }; // 沒有 match 的 event 就會回傳 undefined
+      }
+    });
+
+    const compactEvents = _.compact(events); // 這個函數會 loop array 並過濾掉那些 undefined 的值。我們也可以自己用 filter 寫
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId'); // 這個函數會移除 array 中 duplicate 的那些（要 email + surveyId 一起重複出現才會被移除）
+    */
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.redirect('/surveys/thanks');
   });
 };
